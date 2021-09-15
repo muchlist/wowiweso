a3a2e017799c2f962d0fe689d650d970f33d2556
muchlist <whois.muchlis@gmail.com>
2021-09-16 01:28:12 +0800

    connect to websocket endpoint

---
+++ cmd/web/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/bmizerany/pat"
+	"github.com/muchlist/wowiweso/internal/handlers"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+	return mux
+}
