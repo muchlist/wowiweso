888f0b3fa88a53ff7f9ba600f6baaac6ea79ece7
muchlist <whois.muchlis@gmail.com>
2021-09-24 05:37:49 +0800

    send message

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -115,6 +115,12 @@ func ListenToWsChannel() {
 			users := getUserList()
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+			broadcastToAll(response)
+
 		}
 
 		response.Action = "Got Here"
